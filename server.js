338bfc7a4b42cb2fe2e3e247b8cedca44e99ef38
Aleksei Nepochatykh <Alex.Nepochatyh@gmail.com>
2021-02-16 17:46:47 +0400

    Connected MongoDB

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('db connection is OK');
+  }).catch(err => {
+    console.log(err);
+  });
+
 const app = require('./app');
 
 const PORT = 8888;
