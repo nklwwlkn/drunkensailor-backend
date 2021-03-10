9e374bd0f48831b09e87f5b82713017a875d66a8
Aleksei Nepochatykh <alex.nepochatyh@gmail.com>
2021-03-10 17:34:44 +0300

    upd

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
 
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const ngoRouter = require('./routes/ngoRoutes');
 const projectRouter = require('./routes/projectRoutes');
 
 const app = express();
+
+app.use(helmet)
 
 app.use(express.json());
 
@@ -15,5 +19,10 @@ app.use('/api/v1/ngos', ngoRouter);
 app.use('/api/v1/projects', projectRouter);
 
 app.use(globalErrorHandler);
+
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  });
 
 module.exports = app;
