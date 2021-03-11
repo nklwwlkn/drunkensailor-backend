a14f7a603e58b57f4b1b686107df7e70c565313b
Aleksei Nepochatykh <alex.nepochatyh@gmail.com>
2021-03-11 12:09:02 +0300

    Added home path

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ app.use(helmet())
 
 app.use(express.json());
 
-
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/ngos', ngoRouter);
 app.use('/api/v1/projects', projectRouter);
+
+app.use('/', (req, res, next) => {
+    res.json({
+        "status": "success",
+        "message": "Go away, and never come back."
+    })
+});
+
 
 app.use(globalErrorHandler);
 
